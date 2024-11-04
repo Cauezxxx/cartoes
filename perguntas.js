77777f7510bdb4f46b30f3c9a3825f3e7cf4924e
Cauezxxx <132925128+Cauezxxx@users.noreply.github.com>
2024-11-04 16:16:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é uma variável?',
+    'Uma variável é um espaço na memória que armazena um valor.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Espanha?',
+    'A capital da Espanha é Madri.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um loop?',
+    'Um loop é uma estrutura que repete um bloco de código várias vezes.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz "obrigado" em inglês?',
+    'Se diz "thank you".'
+)
